1fc4b67603fa7db4df4e7ec4c21891d49727e1ff
Edward Zou <zennteam@gmail.com>
2025-07-22 12:21:13 +0800

    Next step: determine the market of the cars sold and deploy llm to group the cars into generations

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ driver = webdriver.Chrome(service=service, options=options)
 
 # Set up base url
 city = "calgary"
+country = "canada" # Used to detemine the market
 base_url = "https://www.facebook.com/marketplace/" + city + "/search?"
 
 # Set up search parameters
